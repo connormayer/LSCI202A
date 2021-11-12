18dbb08e54fa6835ec10aeca7b3341e079b887e5
Connor Mayer <cjmayer@uci.edu>
2021-11-12 13:13:42 -0800

    Commit b1

--- my_file.R
+++ my_file.R
@@ -1,5 +1,5 @@
-my_function <- function(arg1, arg2) {
-	return((arg1 + arg2) ^ 2)
+my_function <- function(arg1, arg2, arg3) {
+	return((arg1 + arg2) ^ arg3)
 }
 
 my_other_function <- function(arg) {
